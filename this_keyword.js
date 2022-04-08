4982d8b6a7926fbadfd9f42824e1bfaf6a0effad
Lucas Hoang Nguyen <hoangknguyen.1807@gmail.com>
2022-04-08 17:42:23 +0700

    function constructor, prototype and class

--- this_keyword.js
+++ this_keyword.js
@@ -6,22 +6,64 @@ function fun() {
 
 // fun.call(person);
 
-function Person(firstName, lastName, age, gender, eyeColor) {
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.age = age;
-	this.gender = gender;
-	this.eyeColor = eyeColor;
+function Person(firstName, lastName, birthYear, gender, eyeColor) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthYear = birthYear;
+  this.gender = gender;
+  this.eyeColor = eyeColor;
+  // this.age = function () {
+  //   return (new Date()).getFullYear() - this.birthYear;
+  // };
+}
+
+Person.prototype.age = function () {
+  return (new Date()).getFullYear() - this.birthYear;
+}
+
+class CPerson {
+  constructor(firstName, lastName, birthYear, gender, eyeColor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.gender = gender;
+    this.eyeColor = eyeColor;
+  }
+
+  get age() {
+    return (new Date()).getFullYear() - this.birthYear;
+  }
+
+  // calcAge() {
+  //   return (new Date()).getFullYear() - this.birthYear;
+  // }
 };
 
-var PersonLambda = (firstName, lastName, age, gender, eyeColor) => ({
-	firstName,
-	lastName,
-	age,
-	gender,
-	eyeColor
-});
+// var PersonLambda = (firstName, lastName, age, gender, eyeColor) => ({
+// 	firstName,
+// 	lastName,
+// 	age,
+// 	gender,
+// 	eyeColor
+// });
 
-const myMate = Person("Albert", "Doherty", 36, "male", "green");
+const myMate1 = new Person("Albert", "Doherty", 1981, "male", "green");
+console.log(myMate1);
+console.log(myMate1.age());
 
-console.log(myMate);
+const myMate2 = new CPerson("Jane", "Watson", 1983, "female", "brown");
+console.log(myMate2);
+console.log(myMate2.age);
+
+console.log(Person.prototype);
+console.log(CPerson.prototype);
+
+// const protoL1 = Object.getPrototypeOf(myMate1);
+// const protoL2 = Object.getPrototypeOf(protoL1);
+// const protoL3 = Object.getPrototypeOf(protoL2);
+// console.log();
+
+// console.log(myMate1);
+// console.log(protoL1);
+// console.log(protoL2);
+// console.log(protoL3);
