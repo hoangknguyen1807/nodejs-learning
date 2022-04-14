e81de6c7107222e07ed7b8af0ca9f11f71446b5a
Lucas Hoang Nguyen <hoangknguyen.1807@gmail.com>
2022-04-14 17:41:55 +0700

    Use Axios to call FTXUS REST API

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 import express from 'express';
+
+import axios from 'axios';
+
+import { createHmac } from 'crypto';
+
+(await import ('dotenv')).config();
+
 const app = express();
 const PORT = 3000;
+
+const ftxAPI = axios.create({
+  baseURL: 'https://ftx.us/api'
+});
+// Alter defaults after instance has been created
+ftxAPI.defaults.headers.common['FTXUS-KEY'] = process.env.FTXUS_API_KEY || '';
+const FTXUS_API_SECRET = process.env.FTXUS_API_SECRET || '';
+
 
 // Without middleware
 app.get('/', function (req, res) {
@@ -16,6 +31,27 @@ app.get('/dmcs', function (req, res) {
 
 app.get('/error', (req, res) => {
 	res.sendStatus(400);
+});
+
+app.get('/ftx/markets', (req, res) => {
+	const ts = (new Date()).valueOf();
+	const method = 'GET';
+	const path = '/markets';
+	const hmac = createHmac('sha256', FTXUS_API_SECRET);
+	hmac.update(ts + method + path);
+	const signature = hmac.digest('hex');
+
+	ftxAPI.get(path, {
+		headers: {
+			'FTXUS-TS': ts,
+			'FTXUS-SIGN': signature
+		}
+	}).then((resp) => {
+		res.status(200).send(resp.data);
+	}, (err) => {
+		console.log(err);
+		res.status(400).send({err: err + ''});
+	});
 });
 
 app.get('*', (req, res) => { 
